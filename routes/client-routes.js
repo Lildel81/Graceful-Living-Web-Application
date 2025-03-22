8b2bed4fe9dbf8576045a2c4201eb21dc697b9df
Terry Weatherman <terry.weatherman8112@gmail.com>
2025-03-22 14:21:19 -0700

    fixed bug in client-routes

--- routes/client-routes.js
+++ routes/client-routes.js
@@ -1,4 +1,4 @@
-const express - require('express');
+const express = require('express');
 const {getHubView,getHomeView,getAssessmentView,getAdminPortalView,getContactView,getResourcesView,getNotFoundView,getServicesView,getShopView} = require ('../controllers/clientController');
 
 
