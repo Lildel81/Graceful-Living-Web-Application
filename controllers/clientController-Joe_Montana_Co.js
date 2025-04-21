072ad00a13096429291e61752f53241b3bcf41f1
Terry Weatherman <terry.weatherman8112@gmail.com>
2025-04-20 21:29:50 -0700

    fixing dependencies

---
+++ controllers/clientController-Joe_Montana_Co.js
@@ -0,0 +1,52 @@
+const Client = require('../models/client'); 
+
+
+
+const getHubView = async (req,res,next) => {
+    res.render('hub');
+}
+
+const getHomeView = async (req, res, next) => {
+    res.render('home');
+}
+
+const getAssessmentView = async (req, res, next) => {
+    res.render('assessment');
+}
+
+const getAdminPortalView = async(req, res, next) => {
+    res.render('adminportal');
+}
+
+const getContactView = async(req, res, next) => {
+    res.render('contact');
+}
+
+const getResourcesView = async(req, res, next) =>{
+    res.render('resources');
+}
+
+const getNotFoundView = async(req, res, next) => {
+    res.render('notFound');
+}
+
+const getServicesView = async(req, res, next) => {
+    res.render('services');
+}
+
+const getShopView = async(req, res, next) => {
+    res.render('shop');
+}
+
+module.exports = { 
+    
+    getHubView,
+    getHomeView,
+    getAssessmentView,
+    getAdminPortalView,
+    getContactView,
+    getResourcesView,
+    getNotFoundView,
+    getServicesView,
+    getShopView,
+ };
