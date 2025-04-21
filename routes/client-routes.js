18ebdd70407ff69ff978bc8599f38f8d5d003343
C0ngH0 <minhho2601@gmail.com>
2025-04-20 22:54:32 -0700

    Add testimonial submission form and success popup to admin portal

--- routes/client-routes.js
+++ routes/client-routes.js
@@ -4,6 +4,7 @@ const {getHubView,getHomeView,getAssessmentView,getAdminPortalView,getContactVie
 
 
 const router = express.Router();
+const Testimonial = require('../models/testimonial');
 
 router.get('/', getHomeView);
 router.get('/hub', getHubView);
@@ -18,6 +19,18 @@ router.get('/application', getApplicationView);
 router.get('/reviews', getReviewsView)
 //router.get('/login', getAdminPortalView); // This is for me to go to adminportal faster
 
+router.post('/admin/add-review', async (req, res) => {
+    try {
+      const { name, quote, location, event } = req.body;
+  
+      await Testimonial.create({ name, quote, location, event });
+  
+      res.redirect('/adminportal?submitted=true');
+    } catch (error) {
+      console.error('Error saving testimonial:', error);
+      res.status(500).send('Something went wrong.');
+    }
+  });
 
 module.exports = {
     routes: router
