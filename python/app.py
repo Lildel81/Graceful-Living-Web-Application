ecaf7ab5a700073097eed0df7cff096a893e3396
Dani Perez <danielaperez20002@gmail.com>
2025-11-10 22:43:59 -0800

    GLWA-269 split data and implemented kfold

---
+++ python/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+from predict_trend import get_prediction
+
+app = Flask(__name__)
+
+@app.route("/api/predict_trend")
+def predict_route():
+    result = get_prediction()
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
